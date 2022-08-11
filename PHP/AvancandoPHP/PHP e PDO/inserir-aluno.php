a98c0aea7a1ef301eac96164e33e24bf4faa5560
César Guilherme <clguilherme3@gmail.com>
2022-08-11 14:47:51 -0300

    Prepared Statements

--- PHP/AvancandoPHP/PHP e PDO/inserir-aluno.php
+++ PHP/AvancandoPHP/PHP e PDO/inserir-aluno.php
@@ -1,14 +1,20 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . 'banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new \Alura\Pdo\Domain\Model\Student(null, 'Cesar Guilherme', new \DateTimeImmutable('1983-05-02'));
+$student = new Student(null, 'Natália Guilherme', new \DateTimeImmutable('1984-12-10'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
-
-var_dump($pdo->exec($sqlInsert));
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+if ($statement->execute()) {
+    echo "Aluno incluído.";
+}
 
 ?>
--- PHP/AvancandoPHP/PHP e PDO/lista-alunos.php
+++ PHP/AvancandoPHP/PHP e PDO/lista-alunos.php
@@ -1,22 +1,21 @@
 <?php
-
-require_once 'vendor/autoload.php';
 use \Alura\Pdo\Domain\Model\Student;
 
-$databasePath = __DIR__ . 'banco.sqlite';
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '\banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $statement = $pdo->query("SELECT * FROM students;");
-
 $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 $studentList = [];
 
 foreach ($studentDataList as $studentData) {
     $studentList[] = new Student(
         $studentData['id'], 
         $studentData['name'], 
-        new \DateTimeImmutable($studentData['birth_date']));
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
 }
 
 var_dump($studentList);
