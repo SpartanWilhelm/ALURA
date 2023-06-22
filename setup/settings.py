897f0f259c9b37ef7fd436d43d67417c09ac702a
César L. Guilherme <clguilherme3@gmail.com>
2023-06-22 09:44:04 -0300

    Localização e Timezone

--- setup/settings.py
+++ setup/settings.py
@@ -15,11 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eq=^7^o_eu7-)lpfv@o8zqha3n7%f(qy$97#6#$_$9=+u9npmj'
+SECRET_KEY = 'django-insecure-47m!v3j15xc=74jd218h%kpy_gge6u3d=g^_hwie!6p64sotzh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
