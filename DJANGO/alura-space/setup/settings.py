4f935f46a276dbbd9d81fcd2764f832b53bb8abf
César L. Guilherme <clguilherme3@gmail.com>
2023-06-22 09:58:08 -0300

    projeto alura space

--- DJANGO/alura-space/setup/settings.py
+++ DJANGO/alura-space/setup/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eq=^7^o_eu7-)lpfv@o8zqha3n7%f(qy$97#6#$_$9=+u9npmj'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
