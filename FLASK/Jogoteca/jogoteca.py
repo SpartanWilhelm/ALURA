15aefdcb5eb3fc3adc905a5338abe3b9d07c078a
César L. Guilherme <clguilherme3@gmail.com>
2024-06-11 15:01:17 -0300

    Montando um formulário no Flask

--- FLASK/Jogoteca/jogoteca.py
+++ FLASK/Jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,20 +6,31 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Yakuza', 'Hack n slash','Xbox One')
+jogo2 = Jogo('Deadfall', 'Aventura', 'PS2')
+jogo3 = Jogo('Mario Kart 7', 'Corrida', '3DS')
+jogo4 = Jogo('Mortal Kombat 1', 'Luta', 'PC')
+
+lista = [jogo1, jogo2, jogo3, jogo4]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    jogo1 = Jogo('Yakuza', 'Hack n slash','Xbox One')
-    jogo2 = Jogo('Deadfall', 'Aventura', 'PS2')
-    jogo3 = Jogo('Mario Kart 7', 'Corrida', '3DS')
-    jogo4 = Jogo('Mortal Kombat 1', 'Luta', 'PC')
-
-    lista = [jogo1, jogo2, jogo3, jogo4]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run(host='0.0.0.0', port=8080)
