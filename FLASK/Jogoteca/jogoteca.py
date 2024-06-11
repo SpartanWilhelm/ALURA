178e7a10e5375fdabfb569e6122bce11b904ae65
César L. Guilherme <clguilherme3@gmail.com>
2024-06-11 13:15:47 -0300

    Mostrando atributos na view

--- FLASK/Jogoteca/jogoteca.py
+++ FLASK/Jogoteca/jogoteca.py
@@ -13,8 +13,9 @@ def ola():
     jogo1 = Jogo('Yakuza', 'Hack n slash','Xbox One')
     jogo2 = Jogo('Deadfall', 'Aventura', 'PS2')
     jogo3 = Jogo('Mario Kart 7', 'Corrida', '3DS')
+    jogo4 = Jogo('Mortal Kombat 1', 'Luta', 'PC')
 
-    lista = [jogo1, jogo2, jogo3]
+    lista = [jogo1, jogo2, jogo3, jogo4]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run(host='0.0.0.0', port=8080)
