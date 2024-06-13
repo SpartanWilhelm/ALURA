b2ec06fad1686e820b8457425f9664ef0765f745
César L. Guilherme <clguilherme3@gmail.com>
2024-06-13 12:46:42 -0300

    Guardando dados na sessão

--- FLASK/Jogoteca/jogoteca.py
+++ FLASK/Jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -14,6 +14,8 @@ jogo4 = Jogo('Mortal Kombat 1', 'Luta', 'PC')
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 app = Flask(__name__)
+
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
@@ -40,8 +42,11 @@ def login():
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
     if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
         return redirect('/')
     else:
+        flash('Usuário não logado!')
         return redirect('/login')
 
 app.run(debug=True, host='0.0.0.0', port=8080)
