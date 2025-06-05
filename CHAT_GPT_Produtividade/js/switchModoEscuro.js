769977b1a39e0dcc86aa62ccf1d10e5b93364937
César L. Guilherme <clguilherme3@gmail.com>
2025-06-05 13:18:20 -0300

    Modo escuro completo

--- CHAT_GPT_Produtividade/js/switchModoEscuro.js
+++ CHAT_GPT_Produtividade/js/switchModoEscuro.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const switchInput = document.querySelector(".cabecalho__switch-input");
     const body = document.body;
+    const logo = document.getElementById('logo-vidflow');
 
     // Verificar estado armazenado no localStorage
     const modoEscuroAtivado = localStorage.getItem("modoEscuro") === "enabled";
@@ -14,9 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
         if (switchInput.checked) {
             body.classList.add("modo-escuro");
             localStorage.setItem("modoEscuro", "enabled"); // Salva a preferência do usuário
+            logo.src = './img/modo_escuro/vidflow-logo-dark-mode.png';
         } else {
             body.classList.remove("modo-escuro");
             localStorage.setItem("modoEscuro", "disabled");
+            logo.src = './img/modo_claro/vidflow-logo-light-mode.png';
         }
     });
 });
