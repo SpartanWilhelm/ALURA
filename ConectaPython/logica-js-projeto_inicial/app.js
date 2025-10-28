c6b4682966abca45abc72d5bf9bd8d49c6c08260
César L. Guilherme <clguilherme3@gmail.com>
2025-10-28 13:02:10 -0300

    Lógica de programação aula 1

---
+++ ConectaPython/logica-js-projeto_inicial/app.js
@@ -0,0 +1,24 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 30:");
+
+if (chute == numeroSecreto) {
+    console.log("Isso aí! Você descobriu o número secreto!");
+}
+
+
+
+/*
+Desafio aula 1
+alert("Boas vindas ao nosso site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Qual a sua idade?");
+if (idade >= 18) {
+    alert("Você pode tirar a habilitação!");
+};
+*/
