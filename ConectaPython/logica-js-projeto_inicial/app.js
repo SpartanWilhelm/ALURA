1d7cf285635e01d29d52d29b61edc4c0cc72c46e
César L. Guilherme <clguilherme3@gmail.com>
2025-10-29 13:05:42 -0300

    Aula 2

--- ConectaPython/logica-js-projeto_inicial/app.js
+++ ConectaPython/logica-js-projeto_inicial/app.js
@@ -1,9 +1,12 @@
-alert("Boas vindas ao jogo do número secreto!");
+alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 30:");
+let chute = prompt('Escolha um número entre 1 e 30:');
 
+//Se chute for igual ao número secreto, exiba uma mensagem de sucesso
 if (chute == numeroSecreto) {
-    console.log("Isso aí! Você descobriu o número secreto!");
+    alert(`Isso aí! Você descobriu o número secreto! ${numeroSecreto}`);
+} else {
+    alert('Que pena! Você errou o número secreto.');
 }
 
 
