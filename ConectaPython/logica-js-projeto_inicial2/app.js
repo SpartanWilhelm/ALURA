4790e2e6d3742a1a207dd4df96f90832a1ce8509
César L. Guilherme <clguilherme3@gmail.com>
2025-11-03 13:12:13 -0300

    Aulas 4 e 5

--- ConectaPython/logica-js-projeto_inicial2/app.js
+++ ConectaPython/logica-js-projeto_inicial2/app.js
@@ -1,14 +1,17 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
+  responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
   exibirTextoNaTela('h1', 'Jogo do número secreto');
-  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e ' + numeroLimite);
 }
 
 exibirMensagemInicial();
@@ -35,7 +38,17 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 10) + 1;
+  let numeroEscolhido = parseInt(Math.random() * numeroLimite) + 1;
+  let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+  if (quantidadeDeElementosNaLista >= 10) {
+    listaDeNumerosSorteados = [];
+  }
+  if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+    return gerarNumeroAleatorio();
+  } else {
+    listaDeNumerosSorteados.push(numeroEscolhido);
+    return numeroEscolhido;
+  }
 }
 
 function limparCampo() {
