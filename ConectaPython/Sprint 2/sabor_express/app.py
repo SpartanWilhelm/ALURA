6d7585d525e6f44dd47a4f2ac557d7841ffde2dd
César L. Guilherme <clguilherme3@gmail.com>
2025-11-12 12:08:57 -0300

    Aula 2

--- ConectaPython/Sprint 2/sabor_express/app.py
+++ ConectaPython/Sprint 2/sabor_express/app.py
@@ -1,14 +1,41 @@
-print ("""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
-print ('1 - Cadastrar Restaurante')
-print ('2 - Listar Restaurante')
-print ('3 - Cadastrar Restaurante')
-print ('4 - Sair\n')
+import os
 
-opcao_escolhida = input('Digite a opção escolhida: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def finalizar_app() :
+    os.system('cls')
+    print('Finalizando o app\n')
+
+def exibir_nome_do_programa():
+    print ("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
+    
+def exibir_opcoes():    
+    print ('1 - Cadastrar Restaurante')
+    print ('2 - Listar Restaurante')
+    print ('3 - Ativar Restaurante')
+    print ('4 - Sair\n')
+
+def escolher_opcao():
+    opcao_escolhida = input('Digite a opção escolhida: ')
+    print(f'Você escolheu a opção {opcao_escolhida}')
+    if opcao_escolhida == '1':
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == '2':
+        print('Listar Restaurante')
+    elif opcao_escolhida == '3':
+        print('Ativar Restaurante')
+    else:    
+        finalizar_app()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
