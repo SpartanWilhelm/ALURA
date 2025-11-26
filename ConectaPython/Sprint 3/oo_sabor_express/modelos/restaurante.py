6eb93b656ca23cf9d2d0df0042771e066f0d9d65
César Leandro Guilherme <clguilherme3@gmail.com>
2025-11-26 13:49:56 -0300

    Aula 3

--- ConectaPython/Sprint 3/oo_sabor_express/modelos/restaurante.py
+++ ConectaPython/Sprint 3/oo_sabor_express/modelos/restaurante.py
@@ -2,20 +2,30 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'Restaurante: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'
-
-    def listar_restaurantes():
+        return f'Restaurante: {self._nome} | Categoria: {self._categoria} | Ativo: {self.ativo}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} |{'Categoria'.ljust(25)}  |Ativo')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
+restaurante_pizza.alternar_estado()
 
 
 Restaurante.listar_restaurantes()
