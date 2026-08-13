9b55d1a7bd3bcf43230cb6c0102a09c6ef333df9
César L. Guilherme <clguilherme3@gmail.com>
2026-08-13 15:21:53 -0300

    Python TDD completo

---
+++ Python e TDD explorando testes unitários/tests/test_bytebank.py
@@ -0,0 +1,61 @@
+import sys, pytest
+
+sys.path.append(r"C:\Users\clgui\source\ALURA\Python e TDD explorando testes unitários")
+
+from codigo.bytebank import Funcionario
+from pytest import mark
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_26(self):
+        #Given-Contexto
+        entrada = '13/03/2000'
+        esperado = 26
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+
+        #When-Quando
+        resultado = funcionario_teste.idade()
+
+        #Then-Desfecho
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho '#Given
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+
+        resultado = lucas.sobrenome() #When
+
+        assert resultado == esperado #Then
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000
+        esperado = 100
+        
+        funcionario_teste = Funcionario('Teste', '11/11/2000', entrada)
+        resultado = funcionario_teste.calcular_bonus()
+        
+        assert resultado == esperado
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_10000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada = 1000000 #given
+            
+            funcionario_teste = Funcionario('Teste', '11/11/2000', entrada)
+            resultado = funcionario_teste.calcular_bonus() #when
+            
+            assert resultado #then
+
